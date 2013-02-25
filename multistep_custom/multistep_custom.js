5405abe946aa9cfec8b464a34e769312e6791f63
Fokat <fokat@fokat-machine.(none)>
2013-02-25 11:30:02 +0530

    first commit of pomc

---
+++ multistep_custom/multistep_custom.js
@@ -0,0 +1,4 @@
+// All js and jQuery for multistep will go here.
+jQuery(function(){
+	   jQuery("#edit-profile-designer-field-prefered-working-location-und-add-button, #edit-profile-designer-field-specialization-und-add-button").click();
+       });
